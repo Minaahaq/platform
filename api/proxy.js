083f37e63c17e6d30e4544962a23e5232fb97d3d
Gocker <ashqmyna85@gmail.com>
2025-11-18 17:43:08 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch(`${process.env.SITE_URL}/api/courses`, {
+    const response = await fetch(${process.env.SITE_URL}/api/courses, {
       headers: {
         "x-api-key": process.env.SECRET_KEY
       }
@@ -8,10 +8,10 @@ export default async function handler(req, res) {
 
     const result = await response.json();
 
+    // <<< هذا السطر هو المهم
     const data = result.data || result;
 
-    // رجّع الداتا بالشكل اللي الواجهة متوقعاه
-    res.status(200).json({ data });
+    res.status(200).json(data);
 
   } catch (error) {
     res.status(500).json({ error: "Proxy Error", details: error.message });
