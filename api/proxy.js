739287956916fbd20f4f8ed8de21f10b3bfab369
Gocker <ashqmyna85@gmail.com>
2025-11-18 13:55:19 +0200

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  try {
+    // استدعاء الـ API الحقيقي مع المفتاح
+    const response = await fetch(`${process.env.VERCEL_URL}/api/courses`, {
+      headers: {
+        "x-api-key": process.env.SECRET_KEY
+      }
+    });
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+
+  } catch (error) {
+    res.status(500).json({ error: "Proxy Error", details: error.message });
+  }
+}
