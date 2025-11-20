d8eca40c5e54cae110e6d3f4128f64ad98e2ec0a
Gocker <ashqmyna85@gmail.com>
2025-11-20 23:57:12 +0200

    Update courses.js

--- api/courses.js
+++ api/courses.js
@@ -1,3 +1,6 @@
+import fs from "fs";
+import path from "path";
+
 export default function handler(req, res) {
   // نقرأ المفتاح اللي جاي في الهيدر
   const secret = req.headers["x-api-key"];
@@ -8,7 +11,13 @@ export default function handler(req, res) {
   }
 
   // جلب البيانات
-  const data = require("../data/coursatk_scraped_data.json");
+  try {
+    const filePath = path.join(process.cwd(), "data/coursatk_scraped_data.json");
+    const jsonData = fs.readFileSync(filePath, "utf-8");
+    const data = JSON.parse(jsonData);
 
-  res.status(200).json({ data });
+    res.status(200).json({ data });
+  } catch (err) {
+    res.status(500).json({ error: "Read Error", details: err.message });
+  }
 }
