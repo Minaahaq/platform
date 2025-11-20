fca8c9049373ece9f75137ced7603b26b2f6d221
Gocker <ashqmyna85@gmail.com>
2025-11-21 00:48:37 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,13 +1,4 @@
 export default async function handler(req, res) {
-
-  const origin = req.headers.origin || "";
-  const allowedOrigin = process.env.SITE_URL;
-
-  // لو الطلب مش جاي من موقعك اقفله
-  if (origin !== allowedOrigin) {
-    return res.status(403).json({ error: "Access Forbidden" });
-  }
-
   try {
     const response = await fetch(`${process.env.SITE_URL}/api/courses`, {
       headers: {
@@ -15,10 +6,15 @@ export default async function handler(req, res) {
       }
     });
 
-    const data = await response.json();
+    const result = await response.json();
+
+    // <<< هذا السطر هو المهم
+    const data = result.data || result;
+
     res.status(200).json(data);
 
   } catch (error) {
     res.status(500).json({ error: "Proxy Error", details: error.message });
   }
 }
+
