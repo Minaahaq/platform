111323b6ca956b2422b29221c579d813bac41241
Gocker <ashqmyna85@gmail.com>
2025-11-20 22:55:13 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,24 +1,19 @@
-// pages/api/proxy.js
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
   try {
-    // الاتصال بالـ API الداخلي مباشرة من السيرفر
-    const internalRes = await fetch(`${process.env.SITE_URL}/api/courses`, {
+    const r = await fetch(`${process.env.SITE_URL}/api/courses`, {
       headers: {
-        "x-api-key": process.env.SECRET_KEY
+        "x-internal-key": process.env.INTERNAL_KEY
       }
     });
 
-    if (!internalRes.ok) {
+    if (!r.ok) {
       return res.status(500).json({ error: "Internal API Error" });
     }
 
-    const result = await internalRes.json();
-    const data = result.data || result;
-
-    // إرسال البيانات للعميل
-    res.status(200).json(data);
+    const result = await r.json();
+    res.status(200).json(result.data || result);
 
   } catch (error) {
     res.status(500).json({ error: "Proxy Error", details: error.message });
