935bc3f3ad99b098ecea6c430cd5aabc26bcc877
Gocker <ashqmyna85@gmail.com>
2025-11-23 21:59:19 +0200

    Update courses.js

--- api/courses.js
+++ api/courses.js
@@ -2,26 +2,6 @@ import crypto from "crypto";
 
 export default function handler(req, res) {
 
-  // ============================
-// 🔥 منع الـ VPN و الـ Proxy
-// ============================
-const ip =
-  req.headers["x-forwarded-for"]?.split(",")[0] ||
-  req.connection.remoteAddress;
-
-try {
-  const vpnCheck = await fetch(`http://ip-api.com/json/${ip}?fields=proxy,hosting`);
-  const info = await vpnCheck.json();
-
-  // لو IP من VPN او Proxy او Hosting Server
-  if (info.proxy || info.hosting) {
-    return res.status(403).json({ error: "VPN Not Allowed" });
-  }
-} catch (err) {
-  // fallback: لو API عطلت → امنع
-  return res.status(403).json({ error: "VPN Check Failed" });
-}
-
 
   // السماح للسيرفر الداخلي فقط
   if (!req.headers["x-vercel-proxy-signature"]) {
